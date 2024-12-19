c737df3300dc008c8de9a0f7c08eb72c77cad3d6
Afonso Lima <afonsolima@gmail.com>
2024-12-19 14:59:12 -0300

    getting form data and creating a new expense object

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
 
 amount.oninput = (event) => {
     let value = amount.value.replace(/\D/g, "");
@@ -15,4 +18,19 @@ function formatCurrencyBRL(value) {
     });
 
     return value;
+}
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
+
+    console.log(newExpense);
 }
